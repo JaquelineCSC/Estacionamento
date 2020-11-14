9593c965e3b092f23dc1f87389b4f7a0d6153828
LeoGarroni <leo-caixeta@hotmail.com>
2020-11-14 14:11:42 -0300

    pull1

---
+++ Estacionamento/EntradaVeiculo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estacionamento
+{
+    public partial class EntradaVeiculo : MetroFramework.Forms.MetroForm
+    {
+        public EntradaVeiculo()
+        {
+            InitializeComponent();
+        }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
--- Estacionamento/FrmPrincipal.cs
+++ Estacionamento/FrmPrincipal.cs
@@ -56,5 +56,11 @@ namespace Estacionamento
             FrmCadastraEstacionamento ce = new FrmCadastraEstacionamento();
             ce.ShowDialog();
         }
+
+        private void entradaVeiculoToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            
+            cc.ShowDialog();
+        }
     }
 }
